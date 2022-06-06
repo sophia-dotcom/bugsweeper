5d7dd1df5cc82fa4af0ce128146076a4ecd9e1ae
sophia123 <sophiachew333@gmail.com>
2022-06-06 13:25:14 +0800

    Mines randomiser done
    changes in my HTML and CSS are not reflected in the browser

--- minesweeper/script.js
+++ minesweeper/script.js
@@ -8,16 +8,21 @@ for (let i = 1; i < totalNumOfMines + 1; i++) {
   mineLocations.push(eachMineLocation);
 }
 console.log(mineLocations);
+
+// removing the repeated element
 for (let i = 0; i < mineLocations.length; i++) {
   for (let j = 0; j < mineLocations.length; j++) {
     if (i === j) {
       continue;
     }
-    // console.log(mineLocations[i]);
     // console.log(mineLocations[j]);
     if (mineLocations[i] === mineLocations[j]) {
-      console.log("Repeated MIne");
       console.log(mineLocations[i]);
+      mineLocations.splice(i, 1);
+      console.log(mineLocations);
+      // generating a new element to replace the removed element
+      mineLocations.push(Math.floor(Math.random() * totalNumOfTiles));
+      console.log(mineLocations);
     }
   }
 }
