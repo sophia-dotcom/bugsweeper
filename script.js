0a4e35cf3fec9bcfb3717f857e83d2e30b97f025
sophia123 <sophiachew333@gmail.com>
2022-06-06 14:10:38 +0800

    more edits to mine randomiser

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-//// Randomise mines and prevent repeats
+/////////////////////////////////////////////
+//// Generating Mines ////
+/////////////////////////////////////////////
+
+// tiles are numbered and mines will be assigned to their correspending number
 const totalNumOfTiles = 9;
 const totalNumOfMines = 3;
 const mineLocations = [];
 
+// generating random numbers to be set as mine's location
 for (let i = 1; i < totalNumOfMines + 1; i++) {
   const eachMineLocation = Math.floor(Math.random() * totalNumOfTiles);
   mineLocations.push(eachMineLocation);
 }
-console.log(mineLocations);
 
-// removing the repeated element
+// removing the repeated elements
 for (let i = 0; i < mineLocations.length; i++) {
   for (let j = 0; j < mineLocations.length; j++) {
     if (i === j) {
       continue;
     }
-    // console.log(mineLocations[j]);
     if (mineLocations[i] === mineLocations[j]) {
-      console.log(mineLocations[i]);
       mineLocations.splice(i, 1);
-      console.log(mineLocations);
-      // generating a new element to replace the removed element
+      // generating new element to replace the removed element
       mineLocations.push(Math.floor(Math.random() * totalNumOfTiles));
-      console.log(mineLocations);
     }
   }
 }
+console.log(mineLocations);
+
+// Setting each mine to correspond to a tile
+for (let i = 0; i < mineLocations.length; i++) {
+  const tile = document.querySelector(`#t${mineLocations[i]}`);
+  tile.className = "tile mine";
+  // tile.style.backgroundImage = 'url("mine30px.jpg")';
+}
+console.log(document.querySelectorAll("span"));
